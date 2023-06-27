228ef526d9175434d42e9599a19852d379455779
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-06-27 17:06:30 +0300

    Write check label on photo

--- routes/notifyRoute.js
+++ routes/notifyRoute.js
@@ -44,31 +44,52 @@ router.post(
   }
 );
 
-router.post('/check', function (req, res) {
-  console.log('/check====>', req.body);
+router.post('/check', async (req, res) => {
+  console.log('/check req.params ====>', req.params);
+  const { photoUrl } = req.body;
 
-  const checkPhoto = async () => {
-    const [result] = await client.safeSearchDetection(
-      // 'https://firebasestorage.googleapis.com/v0/b/telegram-bot-d339c.appspot.com/o/photo%2Fjune%2F1687848306874-girl-avatar.jpg?alt=media&token=01ee4688-cb50-406b-be07-282c9cb6eef2'
-      '../node/uploads/1687845345797 - girl-avatar.jpg'
-    );
+  // check safety
+  const [result] = await client.safeSearchDetection(
+    photoUrl
+    // 'https://firebasestorage.googleapis.com/v0/b/telegram-bot-d339c.appspot.com/o/photo%2Fjune%2F1687848306874-girl-avatar.jpg?alt=media&token=01ee4688-cb50-406b-be07-282c9cb6eef2'
+    // '../node/uploads/1687845345797 - girl-avatar.jpg'
+  );
 
-    const detections = result.safeSearchAnnotation;
-    console.log(`Adult: ${detections.adult}`);
-    console.log(`Spoof: ${detections.spoof}`);
-    console.log(`Medical: ${detections.medical}`);
-    console.log(`Violence: ${detections.violence}`);
+  // https://cloud.google.com/vision/docs/reference/rpc/google.cloud.vision.v1#google.cloud.vision.v1.SafeSearchAnnotation
+  // https://cloud.google.com/vision/docs/reference/rpc/google.cloud.vision.v1#likelihood
+  const detections = result.safeSearchAnnotation;
+  const resCheck = {
+    adult: { chance: detections.adult, desc: 'для дорослих' },
+    spoof: {
+      chance: detections.spoof,
+      desc: 'Імовірність підробки щоб воно виглядало смішним або образливим',
+    },
+    medical: { chance: detections.medical, desc: 'медичне зображення' },
+    violence: { chance: detections.violence, desc: 'насильницький вміст' },
+    racy: { chance: detections.racy, desc: 'непристойний вміст' },
   };
 
-  checkPhoto();
+  // check labels
+  const [labels] = await client.labelDetection(photoUrl);
+  const arrayLabels = labels.labelAnnotations;
+  const descriptions = arrayLabels.map(item => item.description);
 
   res.send({
     status: 'success',
     message: 'File successfully uploaded',
     data: {
-      ...req.body,
+      photoUrl,
+      resCheck,
+      descriptions,
     },
   });
 });
 
 module.exports = { notifyRoutes: router };
+
+// UNKNOWN	Невідома ймовірність.
+// VERY_UNLIKELY	Це дуже малоймовірно.
+// UNLIKELY	Це малоймовірно.
+// POSSIBLE	Можливо.
+// LIKELY	Це ймовірно.
+// VERY_LIKELY	Це дуже ймовірно.
