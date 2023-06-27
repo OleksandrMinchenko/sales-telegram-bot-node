9a700d229b9b70888776251b7e15f677b9db7395
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-06-27 16:00:14 +0300

    ADD google vision api and credentials

--- routes/notifyRoute.js
+++ routes/notifyRoute.js
@@ -3,48 +3,14 @@ const router = express.Router();
 
 const { upload } = require('../middlewares/uploadMiddleware');
 
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: '../node/routes/telegram-bot-d339c-637388e66693.json',
+});
+
 router.get('/', function (req, res) {
   res.send('Hello World');
 });
-
-// router.post('/onefile', function (req, res) {
-//   console.log('req.files ===== >>>> ', req.files);
-//   try {
-//     if (!req.files) {
-//       return res.json({
-//         status: 'failed',
-//         message: 'No file',
-//       });
-//     }
-
-//     let file = req.files.file;
-//     console.log('req.files', req.files);
-//     file.mv('./uploads/' + file.name);
-
-//     res.send({
-//       status: 'success',
-//       message: 'File successfully uploaded',
-//       data: {
-//         name: file.name,
-//         mimetype: file.mimetype,
-//         size: file.size,
-//       },
-//     });
-//   } catch (err) {
-//     res.status(500).send(err);
-//   }
-// });
-
-// router.post('/profile', upload.single('photo1'), function (req, res, next) {
-//   console.log(req.file);
-//   res.send({
-//     status: 'success',
-//     message: 'File successfully uploaded',
-//     data: {
-//       ...req.file,
-//     },
-//   });
-// });
 
 router.post(
   '/photos/upload',
@@ -60,5 +26,49 @@ router.post(
     });
   }
 );
+
+router.post(
+  '/form/upload',
+  upload.array('photos', 5),
+  function (req, res, next) {
+    console.log('/form/upload ====>', req);
+    console.log('/form/upload files ====>', req.body.files);
+    console.log('/form/upload other ====>', req.body.other);
+    res.send({
+      status: 'success',
+      message: 'File successfully uploaded',
+      data: {
+        ...req.files,
+      },
+    });
+  }
+);
+
+router.post('/check', function (req, res) {
+  console.log('/check====>', req.body);
+
+  const checkPhoto = async () => {
+    const [result] = await client.safeSearchDetection(
+      // 'https://firebasestorage.googleapis.com/v0/b/telegram-bot-d339c.appspot.com/o/photo%2Fjune%2F1687848306874-girl-avatar.jpg?alt=media&token=01ee4688-cb50-406b-be07-282c9cb6eef2'
+      '../node/uploads/1687845345797 - girl-avatar.jpg'
+    );
+
+    const detections = result.safeSearchAnnotation;
+    console.log(`Adult: ${detections.adult}`);
+    console.log(`Spoof: ${detections.spoof}`);
+    console.log(`Medical: ${detections.medical}`);
+    console.log(`Violence: ${detections.violence}`);
+  };
+
+  checkPhoto();
+
+  res.send({
+    status: 'success',
+    message: 'File successfully uploaded',
+    data: {
+      ...req.body,
+    },
+  });
+});
 
 module.exports = { notifyRoutes: router };
