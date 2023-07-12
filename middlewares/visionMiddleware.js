52984db8b7aa71051f7318f7fd31aae66593cb07
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-13 02:26:38 +0300

    add forbidden words and new method filter

--- middlewares/visionMiddleware.js
+++ middlewares/visionMiddleware.js
@@ -7,9 +7,16 @@ const { client } = require('../config/vision');
 const visionCheck = async imagePath => {
   let safe,
     label,
+    webDesc,
+    webMatch,
+    attentionWordFirst,
+    attentionWordSecond,
+    attentionWordThird,
     isPermitted = false,
     isPermittedSafe,
-    isPermittedLabel;
+    isPermittedLabel,
+    isPermittedWeb,
+    isPermittedMatch;
 
   try {
     const [result] = await client.safeSearchDetection(imagePath);
@@ -46,26 +53,65 @@ const visionCheck = async imagePath => {
       ],
     };
     const [result] = await client.annotateImage(requests);
-    console.log('result', result);
+    const forbWords = forbiddenValues.join(' | ');
+    const regex = new RegExp(forbWords);
 
     const arrayLabels = result.labelAnnotations;
-    const arrayWebDetection = result.webDetection;
-    console.log(arrayWebDetection);
-    // const arrayLabels = result.labelAnnotations;
-
     label = arrayLabels.map(item => item.description);
-    isPermittedLabel = forbiddenValues.some(item => label.includes(item));
+    const checkLabel = label.join(', ');
+    const findLabel = checkLabel.match(regex);
+    attentionWordFirst = findLabel ? findLabel[0] : null;
+    isPermittedLabel = findLabel ? true : false;
+    console.log('isPermittedLabel', isPermittedLabel);
+
+    const { webEntities, pagesWithMatchingImages } = result.webDetection;
+    webDesc = webEntities.map(item => item.description);
+    const checkWebDesc = webDesc.join(', ');
+    // console.log(checkWebDesc);
+    const findWebDesc = checkWebDesc.match(regex);
+    // console.log(findWebDesc);
+
+    attentionWordSecond = findWebDesc ? findWebDesc[0] : null;
+    isPermittedWeb = findWebDesc ? true : false;
+    console.log('isPermittedWeb', isPermittedWeb);
+
+    webMatch = pagesWithMatchingImages.map(item => item.pageTitle);
+    const checkWeb = webMatch.join(', ');
+    const findMatch = checkWeb.match(regex);
+    attentionWordThird = findMatch ? findMatch[0] : null;
+    isPermittedMatch = findMatch ? true : false;
+    console.log('isPermittedMatch', isPermittedMatch);
   } catch (error) {
     return error.message;
   }
 
-  isPermitted = [isPermittedSafe, isPermittedLabel].includes(true);
+  isPermitted = [
+    isPermittedSafe,
+    isPermittedLabel,
+    isPermittedWeb,
+    isPermittedMatch,
+  ].includes(true);
 
   return {
     imageURL: imagePath,
     isPermitted: !isPermitted,
     safe,
-    label,
+    label: attentionWordFirst
+      ? [`=========> not correct word =======> ${attentionWordFirst}`, ...label]
+      : label,
+    webDesc: attentionWordSecond
+      ? [
+          `=========> not correct word =======> ${attentionWordSecond}`,
+          ...webDesc,
+        ]
+      : webDesc,
+
+    webMatch: attentionWordThird
+      ? [
+          `=========> not correct word =======> ${attentionWordThird}`,
+          ...webMatch,
+        ]
+      : webMatch,
   };
 };
 
--- readme.md
+++ readme.md
@@ -88,3 +88,4 @@
 - [react-loader](https://mhnpd.github.io/react-loader-spinner/docs/components/mutating-dots/)
 - [express.json() and express.urlencoded()](https://stackoverflow.com/questions/23259168/what-are-express-json-and-express-urlencoded)
 - [number-of-labels](https://stackoverflow.com/questions/45259002/google-cloud-vision-api-detects-different-number-of-labels)
+- [Detect Labels, Faces, and Landmarks in Images with the Cloud Vision API](https://www.cloudskillsboost.google/focuses/1841?parent=catalog)
