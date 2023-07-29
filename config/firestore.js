c53d6db740066dc4af646af2e72195b1c158c857
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-29 22:31:27 +0300

    change project id firebase

--- config/firestore.js
+++ config/firestore.js
@@ -1,10 +1,11 @@
 const Firestore = require('@google-cloud/firestore');
 const { STORAGE_PROJECT_KEY } = require('./bucket');
-console.log('STORAGE_PROJECT_KEY', STORAGE_PROJECT_KEY);
+require('dotenv').config();
+// console.log('STORAGE_PROJECT_KEY', STORAGE_PROJECT_KEY);
 
 const db = new Firestore({
   credentials: STORAGE_PROJECT_KEY,
-  projectId: STORAGE_PROJECT_KEY.PROJECT_ID_BUCKET,
+  projectId: process.env.PROJECT_ID_BUCKET,
 });
 
 console.log('db', db);
