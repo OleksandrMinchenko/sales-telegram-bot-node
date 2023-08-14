ac6063591ed04514fd0807826b9ca8854cd635c5
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-08-15 01:00:21 +0300

    add log

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.post('/', async (req, res, next) => {
   const { data, signature } = req.body;
+  console.log('=========> message from bank', `${data}, ===>>>> ${signature}`);
   try {
     res.send({
       status: 'Upload was successful',
